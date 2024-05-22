85c47f5b17f7e4380feb5f8064b58e168e442ef8
jsaavedra <jsaavedra@augetotal.com>
2024-05-22 14:08:50 -0500

    pilot and plane

--- operation/managepilot/managepilot.tpl.php
+++ operation/managepilot/managepilot.tpl.php
@@ -133,7 +133,7 @@ $pilots = $controller->getPilot();
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="editModalLabel">Modificar Vuelo</h5>
+        <h5 class="modal-title" id="editModalLabel">Modificar Piloto</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
--- operation/manageplane/manageplane.tpl.php
+++ operation/manageplane/manageplane.tpl.php
@@ -138,7 +138,7 @@ $planes = $controller->getPlane();
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="editModalLabel">Modificar Vuelo</h5>
+        <h5 class="modal-title" id="editModalLabel">Modificar Avion</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
